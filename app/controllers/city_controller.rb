847d90e12d444ce39fa8b90ab0425682263c800e
kyatul <atul.21j@gmail.com>
2016-06-26 00:47:24 +0530

    Add feature to create City

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -5,4 +5,20 @@ class CityController < ApplicationController
 
   def new
   end
+
+  def create
+    @city = City.new(city_params)
+
+    if @city.save
+      redirect_to admin_cities_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def city_params
+      params.require(:city).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
   get 'admin/cities' => 'city#index'
+  get 'admin/new_city' => 'city#new'
+  post 'admin/new_city' => 'city#create'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
