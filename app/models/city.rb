10c75cb80d188282943e1279f90a239e21be5e4d
kyatul <atul.21j@gmail.com>
2016-06-26 14:10:45 +0530

    Add association for city routes

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,3 @@
 class City < ActiveRecord::Base
+  has_many :routes, class_name: Route, foreign_key: "source_city_id"
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,4 @@
 class Route < ActiveRecord::Base
+  belongs_to :source_city, class_name: City, foreign_key: "source_city_id"
+  belongs_to :destination_city, class_name: City, foreign_key: "destination_city_id"
 end
