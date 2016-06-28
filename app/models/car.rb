73fdd4c63f31c6679d2529615ff9ffc607e07a46
kyatul <atul.21j@gmail.com>
2016-06-28 16:10:55 +0530

    Add association for Cascade destroy

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,3 @@
 class Car < ActiveRecord::Base
+  has_many :available_in_cities, class_name: CarsAvailable, foreign_key: "car_id", dependent: :destroy
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,5 @@
 class City < ActiveRecord::Base
-  has_many :routes, class_name: Route, foreign_key: "source_city_id"
+  has_many :routes, class_name: Route, foreign_key: "source_city_id", dependent: :destroy
+  has_many :routes_as_destination, class_name: Route, foreign_key: "destination_city_id", dependent: :destroy
+  has_many :cars_available, dependent: :destroy
 end
